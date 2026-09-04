038c91ba7bcf9f12db4e5e216535e6acee77bffe
edugcros <98116268+edugcros@users.noreply.github.com>
2026-09-04 15:38:59 -0300

    fix

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -46,13 +46,12 @@ const AiInsightsPage = lazy(() => import('./AiInsightsPage'))
 const StoreSettingsPage = lazy(() => import('./StoreSettingsPage'))
 const VerifyEmailPage = lazy(() => import('./VerifyEmailPage'))
 const MarketIntelligencePage = lazy(() => import('./MarketIntelligencePage'))
-const SubscriptionPage = lazy(() => import('./SubscriptionPage'))
+
 
 const pages = {
   Dashboard,
   EditProduct,
   Login,
-  SubscriptionPage,
   Forgotpassword,
   Resetpassword,
   AiCommercialInboxPage,
